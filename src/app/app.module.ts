204eb09e7c55dbd9acea983894b348cba88818fa
ahmedg99 <gouiaaahma@gmail.com>
2023-02-02 23:08:23 +0100

    create repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InfoComponent } from './info/info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    InfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
